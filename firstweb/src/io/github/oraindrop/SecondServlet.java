05b13d8701c2522d505e31a20ae43c6b747fafad
LAPTOP-E3LTPJD6\ChoiSing <chltmdals115@gmail.com>
2018-08-15 03:22:01 +0900

    forward 실습.
    FrontServlet 에서 SecondServlet으로 포워딩하여 결과를 만드는 로직 구현 실습.

---
+++ firstweb/src/io/github/oraindrop/SecondServlet.java
@@ -0,0 +1,46 @@
+package io.github.oraindrop;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SecondServlet
+ */
+@WebServlet("/second")
+public class SecondServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SecondServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<head><title>form</title></head>");
+        out.println("<body>");
+
+        int dice = (Integer)request.getAttribute("dice");
+        out.println("dice : " + dice);
+        for(int i = 0; i < dice; i++) {
+            out.print("<br>hello");
+        }
+        out.println("</body>");
+        out.println("</html>");
+	}
+
+
+}
